42f595c26f586d18b0adb81aa29ace697a17f25c
CervandoYR <yactayocervando@gmail.com>
2025-03-05 00:52:39 -0500

    cambios

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const app = express();
-app.use(express.json()); // Agregar esta línea
+
+const PORT = process.env.PORT || 3000; // Railway asignará un puerto automáticamente
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando en Railway 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
