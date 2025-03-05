82d57a15de012444ff48d4fe5c9bbaef6cfc2eb4
CervandoYR <yactayocervando@gmail.com>
2025-03-05 00:44:59 -0500

    cambios

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const contactRoutes = require('./routes/contactRoutes');
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/chancay-internet', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
-
-// Routes
-app.use('/api', contactRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ¡¡¡WASSSA!!!!! ${PORT}`);
-});
+app.use(express.json()); // Agregar esta línea
