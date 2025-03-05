496b197c44ae310283815100231ec1a54bb8439c
CervandoYR <yactayocervando@gmail.com>
2025-03-05 01:56:45 -0500

    cambios

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const contactRoutes = require("./routes/contactRoutes");
 require("dotenv").config(); // Cargar variables de entorno
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
